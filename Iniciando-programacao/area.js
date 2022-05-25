b984a1b50190bf52b671302cdb6fc2858df2a920
Luana OLiveira <lulu-so@hotmail.com>
2022-05-25 18:49:37 -0300

    feat(first commit): getting started with programming

---
+++ Iniciando-programacao/area.js
@@ -0,0 +1,8 @@
+// PI * raio * raio = area
+
+const valorPI = Math.PI
+const valorRaio = 10
+
+const valorArea = valorPI * valorRaio * valorRaio
+
+console.log("O valor da área é " + valorArea + "m²")
---
+++ Iniciando-programacao/troca.js
@@ -0,0 +1,12 @@
+var a = 7
+var b = 94
+
+console.log(a)
+console.log(b)
+
+var temp = a
+a = b
+b = temp
+
+console.log(a)
+console.log(b)
