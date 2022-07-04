ff918ceccc8b704f3b0d2b198e1c67f74a283d64
Luana OLiveira <lulu-so@hotmail.com>
2022-07-04 10:09:29 -0300

    feat: the basics of var, let and const

---
+++ fundamentos/variaveisEConstantes.js
@@ -0,0 +1,15 @@
+// variável
+// let é  mais moderno, evite o uso de var
+// var pode sempre ser redeclarada
+var a = 3
+let b = 4
+
+var a = 30
+b = 40
+
+console.log(a, b)
+
+// constante
+const c = 5
+
+console.log(c)
